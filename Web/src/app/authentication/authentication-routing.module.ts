a3c7007791127c79a19f7eb6f0e7420d02ebb1f3
khayrulislam <bsse0822@iit.du.ac.bd>
2019-08-25 16:25:24 +0600

    notfound complete

--- Web/src/app/authentication/authentication-routing.module.ts
+++ Web/src/app/authentication/authentication-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { NotFoundComponent } from '../shared-component/not-found/not-found.component';
 
 
 const routes: Routes = [
@@ -12,9 +11,7 @@ const routes: Routes = [
   {
     path:'sign-up', component: SignUpComponent
   },
-  {
-    path: '**', component: NotFoundComponent
-  }
+
 
 ];
 
