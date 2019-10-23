4d30e884041d7371afb58335be2e1c7bae65e6f8
khayrulislam <bsse0822@iit.du.ac.bd>
2019-10-23 09:09:22 +0600

    remove main and authentication mudule from project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './shared-components/not-found/not-found.component';
-import { AuthComponent } from './authentication/auth/auth.component';
-import { NavigationComponent } from './main/navigation/navigation.component';
+//import { NavigationComponent } from './main/navigation/navigation.component';
 
 
 const routes: Routes = [
-  {
-    path : '', redirectTo:'/auth/account/sign-in', pathMatch : 'full'
-  },
-  {
-    path : 'auth', redirectTo:'/auth/account/sign-in', pathMatch : 'full'
-  },
-  {
-    path:'auth', 
-    component: AuthComponent,
-    loadChildren: () => import('./authentication/authentication.module').then(mod => mod.AuthenticationModule)
-  },
-  {
-    path:'sentiment',
-    component: NavigationComponent,
-    loadChildren: () => import('./main/main.module').then(mod => mod.MainModule)
-  },
+//   {
+//     path : '', redirectTo:'/auth/account/sign-in', pathMatch : 'full'
+//   },
+//   {
+//     path : 'auth', redirectTo:'/auth/account/sign-in', pathMatch : 'full'
+//   },
+//   {
+//     path:'auth', 
+//     component: AuthComponent,
+//     loadChildren: () => import('./authentication/authentication.module').then(mod => mod.AuthenticationModule)
+//   },
+//   {
+//     path:'sentiment',
+//     component: NavigationComponent,
+//     loadChildren: () => import('./main/main.module').then(mod => mod.MainModule)
+//   },
   {
     path: '**', component: NotFoundComponent
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthenticationModule } from './authentication/authentication.module';
 import { NotFoundComponent } from './shared-components/not-found/not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainModule } from './main/main.module';
@@ -16,7 +15,6 @@ import { MainModule } from './main/main.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthenticationModule,
     BrowserAnimationsModule,
     MainModule
   ],
